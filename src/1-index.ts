113a836932a41e27e4c4c39e411b8b4b8613cadc
Sammy <sammylam_1@hotmail.com>
2022-12-06 13:35:30 -0500

    initial

---
+++ src/1-index.ts
@@ -0,0 +1,9 @@
+//variable name: type = declaration
+let age: number = 20;
+//sets age as number and cannot be changed into a string
+//compiles with var
+if (age < 50) {
+	age += 10;
+}
+
+console.log(age);
